a72f34e9c9b0e463b81691146b4b42778fa586eb
kishankishore29 <kishankishore29@gmail.com>
2022-08-26 00:46:59 +0545

    Added gorm dependencies and test code for connecting to database

--- main.go
+++ main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
-  "net/http"
+	"fmt"
 
-  "github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  println("Starting server...")
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	databaseConnectionURL := "postgres://postgres:postgres@localhost:5432/voyager"
+	connection, err := InitializeDatabase(databaseConnectionURL)
+
+	if err != nil {
+		panic("unable to connect to database")
+	}
+
+	fmt.Println("Sucesfully connected to database:", connection)
+}
+
+//InitializeDatabase Connect to the database and apply the migrations. Return the database connection handle.
+func InitializeDatabase(url string) (*gorm.DB, error) {
+
+	// Extract the database configuration using the DATABASE_URL environment variable.
+	parsedURL, err := pq.ParseURL(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Open a connection to postgres
+	database, err := gorm.Open(postgres.Open(parsedURL))
+
+	// Check if there was an error while opening a connection to the database
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the database object
+	return database, nil
 }
